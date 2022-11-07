1a960f9ea2b0f00482cc34bb7a010e7d54624092
Yiğit Aydemir <68844502+st4inl3s5@users.noreply.github.com>
2022-11-07 19:09:04 +0300

    Update

--- util/chat_listener.py
+++ util/chat_listener.py
@@ -8,7 +8,7 @@ colorama.init(autoreset=True)
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-ip", "--ip_address", required=True,help="Set the ip address to listen chat./Chat'i dinlemek icin ip adresi giriniz.")
-ap.add_argument("-p", "--port", required=True,help="Set the port to listen chat./Chat'i dinlemek icin port numarası giriniz.")
+ap.add_argument("-p", "--port", required=True,help="Set the port to listen chat./Chat'i dinlemek icin port numarasi giriniz.")
 args = vars(ap.parse_args())
 
 
